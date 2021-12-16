3cc22b3afa9e9be3f695efaca0536fdab1fa79e4
Hamza <hamzarahman7@gmail.com>
2021-12-16 06:30:39 +0500

    updates

--- views/ContactPage/FormSection.tsx
+++ views/ContactPage/FormSection.tsx
@@ -1,24 +1,24 @@
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import styled from 'styled-components';
-import Button from 'components/Button';
-import Input from 'components/Input';
-import { media } from 'utils/media';
-import MailSentState from '../../components/MailSentState';
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import styled from 'styled-components'
+import Button from 'components/Button'
+import Input from 'components/Input'
+import { media } from 'utils/media'
+import MailSentState from '../../components/MailSentState'
 
 interface EmailPayload {
-  name: string;
-  email: string;
-  description: string;
+  name: string
+  email: string
+  description: string
 }
 
-export default function FormSection() {
-  const [hasSuccessfullySentMail, setHasSuccessfullySentMail] = useState(false);
-  const [hasErrored, setHasErrored] = useState(false);
-  const { register, handleSubmit, formState } = useForm();
-  const { isSubmitSuccessful, isSubmitting, isSubmitted, errors } = formState;
+export default function FormSection () {
+  const [hasSuccessfullySentMail, setHasSuccessfullySentMail] = useState(false)
+  const [hasErrored, setHasErrored] = useState(false)
+  const { register, handleSubmit, formState } = useForm()
+  const { isSubmitSuccessful, isSubmitting, isSubmitted, errors } = formState
 
-  async function onSubmit(payload: EmailPayload) {
+  async function onSubmit (payload: EmailPayload) {
     try {
       const res = await fetch('/api/sendEmail', {
         method: 'POST',
@@ -26,68 +26,68 @@ export default function FormSection() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ subject: 'Email from contact form', ...payload }),
-      });
+      })
 
       if (res.status !== 204) {
-        setHasErrored(true);
+        setHasErrored(true)
       }
     } catch {
-      setHasErrored(true);
-      return;
+      setHasErrored(true)
+      return
     }
 
-    setHasSuccessfullySentMail(true);
+    setHasSuccessfullySentMail(true)
   }
 
-  const isSent = isSubmitSuccessful && isSubmitted;
-  const isDisabled = isSubmitting || isSent;
-  const isSubmitDisabled = Object.keys(errors).length > 0 || isDisabled;
+  const isSent = isSubmitSuccessful && isSubmitted
+  const isDisabled = isSubmitting || isSent
+  const isSubmitDisabled = Object.keys(errors).length > 0 || isDisabled
 
   if (hasSuccessfullySentMail) {
-    return <MailSentState />;
+    return <MailSentState />
   }
 
   return (
     <Wrapper>
       <Form onSubmit={handleSubmit(onSubmit)}>
-        {hasErrored && <ErrorMessage>Couldn&apos;t send email. Please try again.</ErrorMessage>}
+        {hasErrored && <ErrorMessage>Couldn't send email. Please try again.</ErrorMessage>}
         <InputGroup>
           <InputStack>
             {errors.name && <ErrorMessage>Name is required</ErrorMessage>}
-            <Input placeholder="Your Name" id="name" disabled={isDisabled} {...register('name', { required: true })} />
+            <Input placeholder='Your Name' id='name' disabled={isDisabled} {...register('name', { required: true })} />
           </InputStack>
           <InputStack>
             {errors.email && <ErrorMessage>Email is required</ErrorMessage>}
-            <Input placeholder="Your Email" id="email" disabled={isDisabled} {...register('email', { required: true })} />
+            <Input placeholder='Your Email' id='email' disabled={isDisabled} {...register('email', { required: true })} />
           </InputStack>
         </InputGroup>
         <InputStack>
           {errors.description && <ErrorMessage>Description is required</ErrorMessage>}
           <Textarea
-            as="textarea"
-            placeholder="Enter Your Message..."
-            id="description"
+            as='textarea'
+            placeholder='Enter Your Message...'
+            id='description'
             disabled={isDisabled}
             {...register('description', { required: true })}
           />
         </InputStack>
-        <Button as="button" type="submit" disabled={isSubmitDisabled}>
+        <Button as='button' type='submit' disabled={isSubmitDisabled}>
           Send Message
         </Button>
       </Form>
     </Wrapper>
-  );
+  )
 }
 
 const Wrapper = styled.div`
   flex: 2;
-`;
+`
 
 const Form = styled.form`
   & > * {
     margin-bottom: 2rem;
   }
-`;
+`
 
 const InputGroup = styled.div`
   display: flex;
@@ -108,7 +108,7 @@ const InputGroup = styled.div`
       margin-bottom: 2rem;
     }
   }
-`;
+`
 
 const InputStack = styled.div`
   display: flex;
@@ -117,14 +117,14 @@ const InputStack = styled.div`
   & > *:not(:first-child) {
     margin-top: 0.5rem;
   }
-`;
+`
 
 const ErrorMessage = styled.p`
   color: rgb(var(--errorColor));
   font-size: 1.5rem;
-`;
+`
 
 const Textarea = styled(Input)`
   width: 100%;
   min-height: 20rem;
-`;
+`
