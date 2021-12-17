2c277ffef680883a27e13529fb5d39039393df2e
Hamza <hamzarahman7@gmail.com>
2021-12-18 01:25:30 +0500

    other pages added

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,4 +1,3 @@
 export default function Logo ({ theme }) {
-  console.log(theme)
   return theme === 'light' ? <img color='currentColor' width='155' src='/Logo.png' /> : <img width='155' src='/Logo-dark.png' />
 }
---
+++ pages/investor.tsx
@@ -0,0 +1,171 @@
+import { InferGetStaticPropsType } from 'next'
+import Head from 'next/head'
+import styled from 'styled-components'
+import { EnvVars } from 'env'
+import { getAllPosts } from 'utils/postsFetcher'
+import Button from 'components/Button'
+import ButtonGroup from 'components/ButtonGroup'
+import Container from 'components/Container'
+import HeroIllustration from 'components/HeroIllustation'
+import OverTitle from 'components/OverTitle'
+import { useNewsletterModalContext } from 'contexts/newsletter-modal.context'
+import { media } from 'utils/media'
+
+export default function Industry ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const { setIsModalOpened } = useNewsletterModalContext()
+  return (
+    <>
+      <Head>
+        <title>{EnvVars.SITE_NAME}</title>
+        <meta name='description' content='Community of hard science researchers, grad students and entrepreneurs' />
+      </Head>
+      <HomepageWrapper>
+        <WhiteBackgroundContainer>
+          <HeroWrapper>
+            <Contents>
+              <Heading>
+                Leverage our top-tier network of researchers, grad students, entrepreneurs, industry and TTOs focused on commercializing
+                meaningful deeptech research.
+              </Heading>
+              <SignUpText>Sign up here to periodically hear about: </SignUpText>
+              <Description>
+                <ul>
+                  <li>Problem statements from our industry partners</li>
+                  <li>New startups that form within our network</li>
+                  <li>New tech transfer licensing/commercialization opportunities</li>
+                  <li>Promising PhDs/Masters students for contract work or portfolio company hires </li>
+                </ul>
+              </Description>
+
+              <CustomButtonGroup>
+                <Button onClick={() => setIsModalOpened(true)}>
+                  JOIN THE CREW <span>&rarr;</span>
+                </Button>
+              </CustomButtonGroup>
+            </Contents>
+            <ImageContainer>
+              <HeroIllustration />
+            </ImageContainer>
+          </HeroWrapper>
+        </WhiteBackgroundContainer>
+      </HomepageWrapper>
+    </>
+  )
+}
+
+const HomepageWrapper = styled.div`
+  & > :last-child {
+    margin-bottom: 5rem;
+  }
+`
+
+const DarkerBackgroundContainer = styled.div`
+  background: rgb(var(--background));
+
+  & > *:not(:first-child) {
+    margin-top: 15rem;
+  }
+`
+
+const WhiteBackgroundContainer = styled.div`
+  background: rgb(var(--secondBackground));
+
+  & > :last-child {
+    padding-bottom: 5.1rem;
+  }
+
+  & > *:not(:first-child) {
+    margin-top: 15rem;
+  }
+`
+
+const HeroWrapper = styled(Container)`
+  display: flex;
+  padding-top: 5rem;
+
+  ${media('<=desktop')} {
+    padding-top: 1rem;
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
+const Contents = styled.div`
+  flex: 1;
+  max-width: 60rem;
+
+  ${media('<=desktop')} {
+    max-width: 100%;
+  }
+`
+
+const CustomButtonGroup = styled(ButtonGroup)`
+  margin-top: 3rem;
+`
+
+const ImageContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  align-items: flex-start;
+
+  svg {
+    max-width: 45rem;
+  }
+
+  ${media('<=desktop')} {
+    margin-top: 2rem;
+    justify-content: center;
+    svg {
+      max-width: 80%;
+    }
+  }
+`
+
+const Description = styled.div`
+  font-size: 1.9rem;
+  opacity: 0.8;
+  line-height: 1.6;
+
+  ${media('<=desktop')} {
+    font-size: 1.8rem;
+    opacity: 0.8;
+    line-height: 1.6;
+  }
+`
+const SignUpText = styled.p`
+  padding-top: 10px;
+  font-weight: 600;
+  font-size: 2.1rem;
+  line-height: 40px;
+  opacity: 0.8;
+
+  ${media('<=desktop')} {
+    font-size: 2rem;
+  }
+`
+
+const CustomOverTitle = styled(OverTitle)`
+  margin-bottom: 2rem;
+`
+
+const Heading = styled.h1`
+  font-size: 4rem;
+  font-weight: bold;
+  line-height: 1.1;
+  margin-bottom: 4rem;
+  letter-spacing: -0.03em;
+
+  ${media('<=tablet')} {
+    font-size: 4.6rem;
+    margin-bottom: 2rem;
+  }
+`
+
+export async function getStaticProps () {
+  return {
+    props: {
+      posts: await getAllPosts(),
+    },
+  }
+}
