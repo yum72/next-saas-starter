c4f3b82d0e62449007e717ef061d80f738194554
Hamza <hamzarahman7@gmail.com>
2021-12-18 01:38:43 +0500

    logo height

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,3 +1,7 @@
 export default function Logo ({ theme }) {
-  return theme === 'light' ? <img color='currentColor' width='155' src='/Logo.png' /> : <img width='155' src='/Logo-dark.png' />
+  return theme === 'light' ? (
+    <img color='currentColor' width='155' height='33' src='/Logo.png' />
+  ) : (
+    <img width='155' height='33' src='/Logo-dark.png' />
+  )
 }
